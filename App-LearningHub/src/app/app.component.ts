21924397bc76af8cfff74f3659c50dbb4c8e041c
JulienDaCosta-GitHub <55091161+JulienDaCosta-GitHub@users.noreply.github.com>
2020-05-31 02:27:49 +0200

    try debug

--- App-LearningHub/src/app/app.component.ts
+++ App-LearningHub/src/app/app.component.ts
@@ -4,8 +4,6 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import Auth0Cordova from '@auth0/cordova';
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -24,10 +22,6 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
-
-      (window as any).handleOpenURL = (url: string) => {
-        Auth0Cordova.onRedirectUri(url);
-      }
     });
   }
 }
--- App-LearningHub/src/app/app.module.ts
+++ App-LearningHub/src/app/app.module.ts
@@ -5,24 +5,18 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { IonicStorageModule } from '@ionic/storage';
 import { SafariViewController } from '@ionic-native/safari-view-controller/ngx';
 
-import { AuthenticationService } from './services/authentication.service';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AngularFireModule } from '@angular/fire'; // pour se connecter à Firebase
 import { AngularFireDatabaseModule } from '@angular/fire/database'; // pour manipuler la base de données Firebase
 import { AngularFireStorageModule } from '@angular/fire/storage'; // pour accéder aux fonction de stockage et de récupération des fichiers
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { Camera } from '@ionic-native/camera/ngx';
-=======
->>>>>>> parent of 76e22e5... Add Camera
-=======
->>>>>>> parent of 76e22e5... Add Camera
+
 import { environment} from '../environments/environment';
 
 @NgModule({
@@ -31,20 +25,13 @@ import { environment} from '../environments/environment';
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireStorageModule,
-    AngularFireAuthModule,
-    IonicStorageModule.forRoot()],
+    AngularFireAuthModule],
   providers: [
     StatusBar,
     SplashScreen,
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     Camera,
-    AuthenticationService,
     SafariViewController,
-=======
->>>>>>> parent of 76e22e5... Add Camera
-=======
->>>>>>> parent of 76e22e5... Add Camera
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
