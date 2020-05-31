b73507f77cc18e9e58aa12535bb5a18e830bfb3d
lutiku <lutiku.ndongala@edu.devinci.fr>
2020-05-31 16:53:04 +0200

    redirect to 'bienvenue'

--- App-LearningHub/src/app/app-routing.module.ts
+++ App-LearningHub/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'validation',
+    redirectTo: 'bienvenue',
     pathMatch: 'full'
   },
   {
